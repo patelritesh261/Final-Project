2ec6e043a5af10de498ee50c51e59a9f42b253eb
patelritesh261 <patelritesh261@gmail.com>
2016-03-25 19:18:54 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'url': 'mongodb://thomas:12345@ds061345.mongolab.com:61345/heroku_qnl2tjrh'
+};
+
+//# sourceMappingURL=db.js.map
---
+++ server/config/db.ts
@@ -0,0 +1,3 @@
+module.exports = {
+    'url':'mongodb://thomas:12345@ds061345.mongolab.com:61345/heroku_qnl2tjrh'
+};
